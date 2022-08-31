da305c85c3adc4b75c8308e097ddc620b55ad466
Mayank Ahuja <mayankahuja8894@gmail.com>
2022-08-31 23:48:33 +0530

    added firebase performance & analytics monitoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { initializeApp } from "firebase/app";
+import { getPerformance } from "firebase/performance";
+import { getAnalytics } from "firebase/analytics";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,25 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mayank Ahuja';
+
+  firebaseConfig: any = {
+    apiKey: "AIzaSyBIFG4kT-DIbOaBykRSRdXLCD6BC6YNM4I",
+    authDomain: "mayank-ahuja-portfolio.firebaseapp.com",
+    projectId: "mayank-ahuja-portfolio",
+    storageBucket: "mayank-ahuja-portfolio.appspot.com",
+    messagingSenderId: "215587502153",
+    appId: "1:215587502153:web:8a65d5f68fba086608e798",
+    measurementId: "G-2XYD3V7HT8"
+  };
+
+  constructor() {
+    const app = initializeApp(this.firebaseConfig);
+    const analytics = getAnalytics(app);
+    const perf = getPerformance(app);
+  }
 }
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBIFG4kT-DIbOaBykRSRdXLCD6BC6YNM4I",
-//   authDomain: "mayank-ahuja-portfolio.firebaseapp.com",
-//   projectId: "mayank-ahuja-portfolio",
-//   storageBucket: "mayank-ahuja-portfolio.appspot.com",
-//   messagingSenderId: "215587502153",
-//   appId: "1:215587502153:web:8a65d5f68fba086608e798",
-//   measurementId: "G-2XYD3V7HT8"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
