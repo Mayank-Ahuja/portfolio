70b0e5898ef1e983bd0191341359a484a1bed22a
Mayank Ahuja <mayankahuja8894@gmail.com>
2022-08-31 14:59:25 +0530

    updated packages and minor UI fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mayank Ahuja';
 }
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBIFG4kT-DIbOaBykRSRdXLCD6BC6YNM4I",
+//   authDomain: "mayank-ahuja-portfolio.firebaseapp.com",
+//   projectId: "mayank-ahuja-portfolio",
+//   storageBucket: "mayank-ahuja-portfolio.appspot.com",
+//   messagingSenderId: "215587502153",
+//   appId: "1:215587502153:web:8a65d5f68fba086608e798",
+//   measurementId: "G-2XYD3V7HT8"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
